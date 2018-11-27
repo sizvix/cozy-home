b79f000297f967eecef9936dbe66b6432835b8f6
CPatchane <code@patchane.com>
2018-11-27 15:34:34 +0100

    publish: fixfix
    generated from commit 6281d645d48e4cbaa55f845187cec53271cdee29

--- src/components/LoadingPlaceholder.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from '../styles/placeholder'
-
-export const LoadingPlaceholder = () => {
-  return <span className={styles['coz-loading-placeholder']} />
-}
-
-export default LoadingPlaceholder
--- src/lib/triggers.js
+++
@@ -1,8 +0,0 @@
-/* triggers lib ready to be added to cozy-client-js */
-import * as realtime from './realtime'
-
-const TRIGGERS_DOCTYPE = 'io.cozy.triggers'
-
-export function subscribeAll(cozy) {
-  return realtime.subscribeAll(cozy, TRIGGERS_DOCTYPE)
-}
